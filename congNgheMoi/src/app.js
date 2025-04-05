08de2e519640f95a2430a25d7f9a9e171b4d50f5
Pham Thanh Tiến <pttien17323@gmail.com>
2025-04-05 22:02:41 +0700

    oke

--- congNgheMoi/src/app.js
+++ congNgheMoi/src/app.js
@@ -6,8 +6,13 @@ import conversationRoutes from "./routes/conversationRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
-
+import cors from "cors";
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
