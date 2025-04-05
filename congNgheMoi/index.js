a9f8754af7ecf8ec9d527d74def516c0bf646f03
Pham Thanh Tiến <pttien17323@gmail.com>
2025-04-05 13:23:48 +0700

    ok

--- congNgheMoi/index.js
+++ congNgheMoi/index.js
@@ -4,9 +4,18 @@ const { connectDB } = require("./src/configs/connectRDS");
 connectDB();
 
 const PORT = process.env.PORT;
+const cors = require("cors");
+const express = require("express");
 
 const app = require("./src/app");
 
+app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+}))
+app.get("/api/data", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(PORT, (req, res) => {
   console.log(`listen on PORT ${PORT}`);
 });
