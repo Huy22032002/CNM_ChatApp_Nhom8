405227823a1c84f7e4fc45f79df18ee4ee1a4d1a
HoangVuViet <hoangvuviet14@gmail.com>
2025-04-06 20:43:29 +0700

    updateVerifyAccessToken

--- congNgheMoi/src/routes/userRoutes.js
+++ congNgheMoi/src/routes/userRoutes.js
@@ -8,12 +8,12 @@ router.post("/add", createUser);
 
 // Lấy danh sách tất cả người dùng (yêu cầu đăng nhập)
 router.get("/", 
-    authMiddleware, 
+    authMiddlewareWithoutRefresh, 
     getAllUser);
 
 // Cập nhật thông tin người dùng (yêu cầu đăng nhập)
 router.post("/update/:id", 
-    authMiddleware, 
+    authMiddlewareWithoutRefresh, 
     updateUser);
 
 export default router;
