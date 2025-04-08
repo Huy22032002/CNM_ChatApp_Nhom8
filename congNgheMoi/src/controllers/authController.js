683eff3d12e427461db1aff0a159575d04821617
Pham Thanh Tiến <pttien17323@gmail.com>
2025-04-09 01:58:57 +0700

    ok

--- congNgheMoi/src/controllers/authController.js
+++ congNgheMoi/src/controllers/authController.js
@@ -6,7 +6,7 @@ import { findUser, authenticate, updateUser } from "../services/userService.js";
 import { generateToken, verifyAndRefreshToken } from "../configs/jwtConfig.js";
 import sendOtpEmail from "../utils/sendOtpEmail.js";
 import otpCache from "../middlewares/otpCache.js";
-import verifyOtp from "../middlewares/verifyOtp.js";
+// import verifyOtp from "../middlewares/verifyOtp.js";
 
 const register = async (req, res) => {
   try {
@@ -14,7 +14,7 @@ const register = async (req, res) => {
 
     const existingUser = await findUser(username);
     if (existingUser == null) {
-      const hashedPassword = await bcrypt.hash(password, 10);
+      // const hashedPassword = await bcrypt.hash(password, 10);
 
       // Tạo OTP
       const otp = Math.floor(100000 + Math.random() * 900000).toString();
@@ -23,8 +23,19 @@ const register = async (req, res) => {
       // Gửi email OTP
       await sendOtpEmail(email, otp);
 
+      // const hashedPassword = await bcrypt.hash(password, 10);//for testing
+      // const newUser = new User({
+      //   username,
+      //   pass_hash: hashedPassword,
+      //   email,
+      //   phone,
+      // });
+      // await newUser.save();//for testing
+
+
       // Chưa lưu user vào DB ngay — đợi xác thực OTP
       res.status(200).json({ message: "OTP sent to email", email });
+      return otp;
     } else {
       return res.status(400).json({ message: "Username already exists" });
     }
@@ -33,9 +44,11 @@ const register = async (req, res) => {
   }
 };
 
-const verifyOtp = async (req, res) => {
+
+const verifyOtp= async (req, res) => {
     const { email, username, password, phone, otp } = req.body;
     const storedOtp = otpCache.get(email);
+    
   
     if (storedOtp !== otp) {
       return res.status(400).json({ message: "OTP không chính xác" });
@@ -55,35 +68,6 @@ const verifyOtp = async (req, res) => {
     res.status(201).json({ message: "Đăng ký thành công" });
 };
 
-// const register = async (req, res) => {
-//   try {
-//     const { username, password, email, phone } = req.body;
-
-
-//         // Check if user already exists
-//         const existingUser = await findUser(username);
-//         if (existingUser==null) {
-//             const hashedPassword = await bcrypt.hash(password,10);
-//         // console.log(hashedPassword);
-
-//       // Create a new user
-//       const newUser = new User({
-//         username,
-//         pass_hash: hashedPassword,
-//         email,
-//         phone,
-//       });
-//       await newUser.save();
-
-//       res.status(201).json({ message: "User registered successfully" });
-//     } else {
-//       return res.status(400).json({ message: "Username already exists" });
-//     }
-//     // Hash the password
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error", error });
-//   }
-// };
 
 const login = async (req, res) => {
 
