7dc217984f0703b78b43bcaf75108dfd871e6223
Huy22032002 <ndhuy2203@gmail.com>
2025-04-14 14:23:39 +0700

    update, delete, revoke in Message Service

--- congNgheMoi/src/services/messageService.js
+++ congNgheMoi/src/services/messageService.js
@@ -6,7 +6,7 @@ const MessageService = {
       return await MessageModel.createMessage(message);
     } catch (error) {
       console.error(`Err creating message: ${error.message}`);
-      throw new Error("Err creating message service");
+      throw new Error(`Err creating message service: ${error.message}`);
     }
   },
   async getAllMessageByConversationId(conversation_id) {
@@ -17,15 +17,41 @@ const MessageService = {
       return await MessageModel.getAllMessageByConversationId(conversation_id);
     } catch (error) {
       console.error(`Error fetching messages: ${error.message}`);
-      throw new Error("Error fetching messages service");
+      throw new Error(`Error fetching messages service: ${error.message}`);
     }
   },
-  async updateMessageContent(message) {
+  async updateMessageContent(message_id, user_id, content) {
     try {
-      return await MessageModel.updateMessageContent(message);
+      return await MessageModel.updateMessageContent(
+        message_id,
+        user_id,
+        content
+      );
     } catch (err) {
-      console.log(`Err update message content service ${err}`);
-      throw new Error("Err update msg content serivce");
+      console.error(`Err update message content service ${err.message}`);
+      throw new Error(`Err update msg content serivce: ${err.message}`);
+    }
+  },
+  async deleteMessage(message_id, user_id) {
+    try {
+      await MessageModel.deleteMessage(message_id, user_id);
+    } catch (err) {
+      console.error(`err delete message in message service: ${err.message}`);
+      throw new Error(`err delete message in message service: ${err.message}`);
+    }
+  },
+  async revokeMessage(message_id, user_id) {
+    try {
+      const revokedMessage = await MessageModel.revokeMessage(
+        message_id,
+        user_id
+      );
+      return revokedMessage;
+    } catch (err) {
+      console.error(`error revoke message in message service: ${err}`);
+      throw new Error(
+        `Error revok message in message service: ${err.message} `
+      );
     }
   },
 };
